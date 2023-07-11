02fa85d739240ffe6c2c676193c18b7763724f80
GHITADAHIBI <dahibighita@gmail.com>
2023-07-11 15:07:18 +0100

    firstcommit

---
+++ reactpca-master/reactpca-master/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import FileForm from '../../reactpca-master/src/SharedComponents/CardsJobs/Jobs'
+function App() {
+  return (
+    <div className="App">
+    
+    </div>
+  );
+}
+
+export default App;
