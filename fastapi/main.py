8d002ceb6009ac8bc434cbfc8ab221d7564c94a9
GHITADAHIBI <dahibighita@gmail.com>
2023-07-11 15:51:37 +0100

    commit

--- fastapi/main.py
+++ fastapi/main.py
@@ -20,3 +20,16 @@ async def create_upload_file(file_upload: UploadFile):
     with open(save_to, 'wb') as f:
         f.write(data)
     return {"filenames": file_upload.filename}
+
+
+
+from pymongo.mongo_client import MongoClient
+uri = "mongodb+srv://dahibighita:24682468@cluster0.jgxb5jw.mongodb.net/?retryWrites=true&w=majority"
+# Create a new client and connect to the server
+client = MongoClient(uri)
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
